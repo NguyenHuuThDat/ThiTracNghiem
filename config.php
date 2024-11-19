f2609351063d52515711393aac60c9d3af76179a
NguyenHuuThDat <huudatnh178@gmail.com>
2024-11-19 21:55:15 +0700

    add config

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define("host_name", "localhost");
+define("db_user", "root");
+define("db_password", "");
+define("db_name", "thitracnghiem");
+define("app_path", "/Thitracnghiem/");
+define("login_path", "http://localhost/Thitracnghiem/auth/signin");
