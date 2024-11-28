1c4de572a5421a8bcca1ebb522970af3582d0aa0
Sotatek-NhiNguyen2 <174430306+Sotatek-NhiNguyen2@users.noreply.github.com>
2024-11-28 08:55:44 +0700

    feat: add test detail and test result model

---
+++ mvc/models/KetQuaModel.php
@@ -0,0 +1,12 @@
+<?php
+class KetQuaModel extends DB{
+    // 1. Phương thức khởi tạo kết quả
+    public function start($made, $manguoidung)
+    {
+        $valid = true;
+        $sql = "INSERT INTO `ketqua`(`made`, `manguoidung`) VALUES ('$made','$manguoidung')";
+        $result = mysqli_query($this->con, $sql);
+        if(!$result) $valid = false;
+        return $valid;
+    }
+}
