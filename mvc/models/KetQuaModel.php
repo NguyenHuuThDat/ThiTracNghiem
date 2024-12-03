4357ae70fb443b380010afd452c05a7916c1728f
Sotatek-NhiNguyen2 <174430306+Sotatek-NhiNguyen2@users.noreply.github.com>
2024-12-03 14:50:24 +0700

    feat(3/12): update test result

--- mvc/models/KetQuaModel.php
+++ mvc/models/KetQuaModel.php
@@ -9,4 +9,51 @@ class KetQuaModel extends DB{
         if(!$result) $valid = false;
         return $valid;
     }
+
+    public function updateChangeTab($made, $manguoidung)
+    {
+        $solanchuyentab = $this->getChangeTab($made, $manguoidung)['solanchuyentab'];
+        $sql = "UPDATE `ketqua` SET `solanchuyentab`='$solanchuyentab' WHERE `made`='$made' AND `manguoidung`='$manguoidung'";
+        $valid = true;
+        $result = mysqli_query($this->con, $sql);
+        if(!$result) $valid = false;
+        return $valid;
+    }
+
+    public function getChangeTab($made, $manguoidung) {
+        $sql = "SELECT `solanchuyentab` FROM `ketqua` WHERE `made`='$made' AND `manguoidung`='$manguoidung'";
+        $result = mysqli_query($this->con, $sql);
+        return mysqli_fetch_assoc($result);
+    }
+
+    public function getMaKQ($made, $manguoidung)
+    {
+        $sql = "SELECT * FROM `ketqua` WHERE `made` = '$made' AND `manguoidung` = '$manguoidung'";
+        $result = mysqli_query($this->con, $sql);
+        return mysqli_fetch_assoc($result);
+    }
+
+    public function submit($made,$nguoidung,$list,$thoigian){
+        $sql_ketqua = "Select * from ketqua where made = '$made' and manguoidung = '$nguoidung'";
+        $result_ketqua = mysqli_query($this->con,$sql_ketqua);
+        $data = mysqli_fetch_assoc($result_ketqua);
+        $thoigianvaolam = strtotime($data['thoigianvaothi']);
+        $thoigianlambai = strtotime($thoigian) - $thoigianvaolam;
+        $valid = true;
+        $socaudung = $this->socaudung($list);
+        $socau = count($list);
+        $diem = round((10/$socau * $socaudung),2);
+        $sql = "UPDATE `ketqua` SET `diemthi`='$diem',`thoigianlambai`='$thoigianlambai',`socaudung`='$socaudung' WHERE manguoidung = '$nguoidung' and made = '$made'";
+        $result = mysqli_query($this->con,$sql);
+        if(!$result) $valid = false;
+        $makq = $data['makq'];
+        foreach($list as $ct){
+            $macauhoi = $ct['macauhoi'];
+            $cautraloi = $ct['cautraloi'];
+            $sql = "UPDATE `chitietketqua` SET `dapanchon`='$cautraloi' WHERE `makq`='$makq' AND `macauhoi`='$macauhoi'";
+            $insertCt = mysqli_query($this->con,$sql);
+            if(!$insertCt) $valid = false;
+        }
+        return $valid;
+    }
 }
