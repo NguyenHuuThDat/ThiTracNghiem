c531bce0a4cf3408d59737eb657e6d93845daaf6
Sotatek-NhiNguyen2 <174430306+Sotatek-NhiNguyen2@users.noreply.github.com>
2024-12-06 16:20:03 +0700

    Add test management and processing functionalities

--- mvc/controllers/test.php
+++ mvc/controllers/test.php
@@ -173,4 +173,248 @@ class Test extends Controller
             $this->view("single_layout", ["Page" => "error/page_404", "Title" => "Lỗi !"]);
         }
     }
+
+    // Tham gia thi
+    public function taketest($made)
+    {
+        if(filter_var($made, FILTER_VALIDATE_INT) !== false) {
+            if (AuthCore::checkPermission("tgthi", "join")) {
+                $user_id = $_SESSION['user_id'];
+                $check = $this->ketquamodel->getMaKQ($made, $user_id);
+                $infoTest = $this->dethimodel->getById($made);
+                date_default_timezone_set('Asia/Ho_Chi_Minh');
+                $now = new DateTime();
+                $timestart = new DateTime($infoTest['thoigianbatdau']);
+                $timeend = new DateTime($infoTest['thoigianketthuc']);
+                if ($now >= $timestart && $now <= $timeend && $check['diemthi'] == '') {
+                    $this->view("single_layout", [
+                        "Page" => "de_thi",
+                        "Title" => "Làm bài kiểm tra",
+                        "Made" => $made,
+                        "Script" => "de_thi",
+                        "Plugin" => [
+                            "sweetalert2" => 1
+                        ]
+                    ]);
+                } else {
+                    header("Location: ../start/$made");
+                }
+            } else {
+                $this->view("single_layout", ["Page" => "error/page_403", "Title" => "Lỗi !"]);
+            }
+        } else {
+            $this->view("single_layout", ["Page" => "error/page_404", "Title" => "Lỗi !"]);
+        }
+    }
+
+    public function delete()
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && AuthCore::checkPermission("dethi", "delete")) {
+            $made = $_POST['made'];
+            $result = $this->dethimodel->delete($made);
+            echo json_encode($result);
+        } else {
+            echo json_encode(false);
+        }
+    }
+
+    public function addTest()
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && AuthCore::checkPermission("dethi", "create")) {
+            $mamonhoc = $_POST['mamonhoc'];
+            $nguoitao = $_SESSION['user_id'];
+            $tende = $_POST['tende'];
+            $thoigianthi = $_POST['thoigianthi'];
+            $thoigianbatdau = $_POST['thoigianbatdau'];
+            $thoigianketthuc = $_POST['thoigianketthuc'];
+            $socaude = $_POST['socaude'];
+            $socautb = $_POST['socautb'];
+            $socaukho = $_POST['socaukho'];
+            $chuong = isset($_POST['chuong']) ? $_POST['chuong'] : array();
+            $loaide = $_POST['loaide'];
+            $xemdiem = $_POST['xemdiem'];
+            $xemdapan = $_POST['xemdapan'];
+            $xembailam = $_POST['xembailam'];
+            $daocauhoi = $_POST['daocauhoi'];
+            $daodapan = $_POST['daodapan'];
+            $tudongnop = $_POST['tudongnop'];
+            $manhom = $_POST['manhom'];
+            $result = $this->dethimodel->create($mamonhoc, $nguoitao, $tende, $thoigianthi, $thoigianbatdau, $thoigianketthuc, $xembailam, $xemdiem, $xemdapan, $daocauhoi, $daodapan, $tudongnop, $loaide, $socaude, $socautb, $socaukho, $chuong, $manhom);
+            echo $result;
+        }
+    }
+
+    public function updateTest()
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && AuthCore::checkPermission("dethi", "update")) {
+            $made = $_POST['made'];
+            $mamonhoc = $_POST['mamonhoc'];
+            $tende = $_POST['tende'];
+            $thoigianthi = $_POST['thoigianthi'];
+            $thoigianbatdau = $_POST['thoigianbatdau'];
+            $thoigianketthuc = $_POST['thoigianketthuc'];
+            $socaude = $_POST['socaude'];
+            $socautb = $_POST['socautb'];
+            $socaukho = $_POST['socaukho'];
+            $chuong = $_POST['chuong'];
+            $loaide = $_POST['loaide'];
+            $xemdiem = $_POST['xemdiem'];
+            $xemdapan = $_POST['xemdapan'];
+            $xembailam = $_POST['xembailam'];
+            $daocauhoi = $_POST['daocauhoi'];
+            $daodapan = $_POST['daodapan'];
+            $tudongnop = $_POST['tudongnop'];
+            $manhom = $_POST['manhom'];
+            $result = $this->dethimodel->update($made, $mamonhoc, $tende, $thoigianthi, $thoigianbatdau, $thoigianketthuc, $xembailam, $xemdiem, $xemdapan, $daocauhoi, $daodapan, $tudongnop, $loaide, $socaude, $socautb, $socaukho, $chuong, $manhom);
+            echo $result;
+        }
+    }
+
+    public function getDetail()
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && AuthCore::checkPermission("dethi", "view")) {
+            $made = $_POST['made'];
+            $result = $this->dethimodel->getById($made);
+            echo json_encode($result);
+        }
+    }
+
+    public function getTestGroup()
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $manhom = $_POST['manhom'];
+            $result = $this->dethimodel->getListTestGroup($manhom);
+            echo json_encode($result);
+        }
+    }
+
+    public function addDetail()
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $made = $_POST['made'];
+            $cauhoi = $_POST['cauhoi'];
+            $result = $this->chitietde->createMultiple($made, $cauhoi);
+            echo json_encode($result);
+        }
+    }
+
+    public function getQuestion()
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $made = $_POST['made'];
+            $result = $this->dethimodel->getQuestionByUser($made, $_SESSION['user_id']);
+            echo json_encode($result);
+        }
+    }
+
+    public function getQuestionOfTestManual()
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $made = $_POST['made'];
+            $result = $this->dethimodel->getQuestionOfTestManual($made);
+            echo json_encode($result);
+        }
+    }
+
+    public function getResultDetail()
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $makq = $_POST['makq'];
+            $result = $this->dethimodel->getResultDetail($makq);
+            echo json_encode($result);
+        }
+    }
+
+
+    public function startTest()
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $made = $_POST['made'];
+            $user_id = $_SESSION['user_id'];
+            $result = $this->ketquamodel->start($made, $user_id);
+            $question = $this->dethimodel->getQuestionOfTest($made);
+            echo json_encode($result);
+        }
+    }
+
+    public function getTimeTest()
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $dethi = $_POST['dethi'];
+            $result = $this->dethimodel->getTimeTest($dethi, $_SESSION['user_id']);
+            echo $result;
+        }
+    }
+
+    public function getTimeEndTest()
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $dethi = $_POST['dethi'];
+            $result = $this->dethimodel->getTimeEndTest($dethi);
+            echo $result;
+        }
+    }
+
+    public function submit()
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $listtr = $_POST['listCauTraLoi'];
+            $thoigian = $_POST['thoigianlambai'];
+            str_replace("(Indochina Time)", "(UTC+7:00)", $thoigian);
+            $date = DateTime::createFromFormat('D M d Y H:i:s e+', $thoigian);
+            $made = $_POST['made'];
+            $nguoidung = $_SESSION['user_id'];
+            $result = $this->ketquamodel->submit($made, $nguoidung, $listtr, $date->format('Y-m-d H:i:s'));
+            echo $result;
+        }
+    }
+
+    public function getDethi()
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $dethi = $_POST['made'];
+            $result = $this->dethimodel->create_dethi($dethi);
+            echo json_encode($result);
+        }
+    }
+
+    public function tookTheExam()
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $made = $_POST['made'];
+            $result = $this->ketquamodel->tookTheExam($made);
+            echo json_encode($result);
+        }
+    }
+
+    public function getExamineeByGroup()
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $made = $_POST['made'];
+            $manhom = $_POST['manhom'];
+            $result = $this->ketquamodel->getExamineeByGroup($made, $manhom);
+            echo json_encode($result);
+        }
+    }
+
+    public function getQuery($filter, $input, $args)
+    {
+        $result = $this->ketquamodel->getQuery($filter, $input, $args);
+        return $result;
+    }
+
+    public function getStatictical()
+    {
+        $made = $_POST['made'];
+        $manhom = $_POST['manhom'];
+        $result = $this->ketquamodel->getStatictical($made, $manhom);
+        echo json_encode($result);
+    }
+
+    public function chuyentab(){
+        $made = $_POST['made'];
+        $id = $_SESSION['user_id'];
+        $result = $this->ketquamodel->chuyentab($made,$id);
+        echo $result;
+    }
 }
