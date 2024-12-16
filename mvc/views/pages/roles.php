f5443afc805273279107f9656c701acaec43f1e6
ThanhHien0903 <hien.tranthanh1109@gmail.com>
2024-12-16 23:06:10 +0700

    add roles

---
+++ mvc/views/pages/roles.php
@@ -0,0 +1,9 @@
+<div class="content">
+    <div class="block block-rounded">
+        <div class="block-header block-header-default">
+            <h3 class="block-title">Danh sách nhóm quyền</h3>
+            <div class="block-options">
+                <button data-role="nhomquyen" data-action="create" type="button" class="btn btn-hero btn-primary" data-bs-toggle="modal"
+                    data-bs-target="#modal-add-role"><i class="fa-regular fa-plus me-1"></i> Thêm mới</button>
+            </div>
+        </div>
