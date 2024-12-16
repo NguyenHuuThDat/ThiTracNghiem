fd170690bebe6fcf207c73794a495b35e893f4da
ThanhHien0903 <hien.tranthanh1109@gmail.com>
2024-12-16 23:18:11 +0700

    update role

--- mvc/views/pages/roles.php
+++ mvc/views/pages/roles.php
@@ -7,3 +7,16 @@
                     data-bs-target="#modal-add-role"><i class="fa-regular fa-plus me-1"></i> Thêm mới</button>
             </div>
         </div>
+        <div class="block-content">
+            <div class="row justify-content-center">
+                <div class="col-md-11">
+                    <form action="be_pages_ecom_orders.html" method="POST" onsubmit="return false;">
+                        <div class="mb-4">
+                            <div class="input-group">
+                                <input type="text" class="form-control form-control-alt" id="one-ecom-orders-search"
+                                    name="one-ecom-orders-search" placeholder="Tìm kiếm nhóm quyền..">
+                                <span class="input-group-text bg-body border-0">
+                                    <i class="fa fa-search"></i>
+                                </span>
+                            </div>
+                        </div>
