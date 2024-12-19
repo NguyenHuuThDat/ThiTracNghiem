a328b6b0712653f96c35f8fcfe1de8695fca3e2a
Nguyễn Hữu Thành Đạt <huudatnh178@gmail.com>
2024-12-19 23:43:01 +0700

    sua js landing

--- public/js/pages/landing.js
+++ public/js/pages/landing.js
@@ -2,35 +2,37 @@ Dashmix.helpersOnLoad(['jq-appear']);
 Dashmix.helpersOnLoad(['jq-slick'])
 
 $('.slider-team').slick({
-  autoplay: true, // Set the autoplay option based on the data attribute
-  dots: true, // Set the dots option based on the data attribute
-  arrows: true, // Set the arrows option based on the data attribute
-  slidesToShow: 4, // Set the slidesToShow option based on the data attribute
-  responsive: [
-    {
-      breakpoint: 1024, // Add breakpoints for different screen sizes
-      settings: {
-        slidesToShow: 3 // Change the number of slides to show for this screen size
-      }
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1
-      }
-    }
-  ]
+    autoplay: true, // Đặt tùy chọn phát tự động dựa trên thuộc tính dữ liệu
+    dots: true, // Đặt tùy chọn chấm dựa trên thuộc tính dữ liệu
+    arrows: true, // Đặt tùy chọn mũi tên dựa trên thuộc tính dữ liệu
+    slidesToShow: 4, // Đặt tùy chọn Hiển thị trang chiếu dựa trên thuộc tính dữ liệu
+    responsive: [
+        {
+            breakpoint: 1024, // Thêm điểm ngắt cho các kích thước màn hình khác nhau
+            settings: {
+                slidesToShow: 3 // Thay đổi số lượng slide hiển thị cho kích thước màn hình này
+            }
+        },
+
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+    ]
 });
 
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.getElementById("section--1");
 
 btnScrollTo.addEventListener('click', function () {
-  section1.scrollIntoView({ behavior: 'smooth' });
+    section1.scrollIntoView({ behavior: 'smooth' });
 });
