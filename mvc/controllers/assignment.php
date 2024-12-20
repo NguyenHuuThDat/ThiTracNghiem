a08ff5dc5686e7a1d5b622634726b70122861e38
Nguyễn Hữu Thành Đạt <huudatnh178@gmail.com>
2024-12-20 08:47:44 +0700

    sua controll assignment

--- mvc/controllers/assignment.php
+++ mvc/controllers/assignment.php
@@ -1,18 +1,15 @@
 <?php
-class Assignment extends Controller
-{
+class Assignment extends Controller {
     public $PhanCongModel;
 
-    function __construct()
-    {
+    function __construct() {
         $this->PhanCongModel = $this->model("PhanCongModel");
         parent::__construct();
         require_once "./mvc/core/Pagination.php";
     }
 
-    function default()
-    {
-        if (AuthCore::checkPermission("phancong", "view")) {
+    function default() {
+        if(AuthCore::checkPermission("phancong","view")) {
             $this->view("main_layout", [
                 "Page" => "assignment",
                 "Title" => "Phân quyền",
@@ -26,79 +23,71 @@ class Assignment extends Controller
                 ],
                 "Script" => "assignment"
             ]);
-        } else $this->view("single_layout", ["Page" => "error/page_403", "Title" => "Lỗi !"]);
+        } else $this->view("single_layout", ["Page" => "error/page_403","Title" => "Lỗi !"]);
     }
 
-    function getGiangVien()
-    {
+    function getGiangVien(){
         AuthCore::checkAuthentication();
-        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+        if($_SERVER["REQUEST_METHOD"] == "GET"){
             $result = $this->PhanCongModel->getGiangVien();
             echo json_encode($result);
         }
     }
 
-    function getMonHoc()
-    {
+    function getMonHoc(){
         AuthCore::checkAuthentication();
-        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+        if($_SERVER["REQUEST_METHOD"] == "GET"){
             $result = $this->PhanCongModel->getMonHoc();
             echo json_encode($result);
         }
     }
 
-    function getAssignment()
-    {
+    function getAssignment(){
         AuthCore::checkAuthentication();
-        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+        if($_SERVER["REQUEST_METHOD"] == "GET"){
             $result = $this->PhanCongModel->getAssignment();
             echo json_encode($result);
         }
     }
 
-    function addAssignment()
-    {
+    function addAssignment(){
         AuthCore::checkAuthentication();
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
             $magiangvien = $_POST['magiangvien'];
             $l_subject = $_POST['listSubject'];
-            $result = $this->PhanCongModel->addAssignment($magiangvien, $l_subject);
+            $result = $this->PhanCongModel->addAssignment($magiangvien,$l_subject);
             echo $result;
         }
     }
 
-    function delete()
-    {
+    function delete(){
         AuthCore::checkAuthentication();
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
             $id = $_POST['id'];
             $mamon = $_POST['mamon'];
-            $result = $this->PhanCongModel->delete($mamon, $id);
+            $result = $this->PhanCongModel->delete($mamon,$id);
             echo $result;
         }
     }
 
-    function deleteAll()
-    {
+    function deleteAll(){
         AuthCore::checkAuthentication();
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
             $id = $_POST['id'];
             $result = $this->PhanCongModel->deleteAll($id);
         }
     }
 
-    function getAssignmentByUser()
-    {
+    function getAssignmentByUser(){
         AuthCore::checkAuthentication();
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
             $id = $_POST['id'];
             $result = $this->PhanCongModel->getAssignmentByUser($id);
             echo json_encode($result);
         }
     }
 
-    public function getQuery($filter, $input, $args)
-    {
+    public function getQuery($filter, $input, $args) {
         AuthCore::checkAuthentication();
         $query = $this->PhanCongModel->getQuery($filter, $input, $args);
         return $query;
